d9f137f1be65f6b473454b780988f9aadacdae58
swxctx <swxctx@gmail.com>
2018-12-20 20:30:01 +0800

    标签读取转换

---
+++ format.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"strings"
+	"unicode"
+)
+
+func (xj *xjson) keyFilter(str string) (string, string) {
+	temp := xj.keyCase(str)
+	if _, ok := xj.MapTag[temp]; !ok {
+		xj.MapTag[temp] = str
+	}
+	if xj.JSONTag {
+		return str, temp
+	}
+	xj.MapTag[temp] = ""
+	return str, temp
+
+}
+
+//change name to different case
+func (xj *xjson) keyCase(str string) string {
+	temp := strings.FieldsFunc(str, xj.XJSplit)
+	var (
+		upperStr string
+	)
+	for y := 0; y < len(temp); y++ {
+		x := []rune(temp[y])
+		for i := 0; i < len(x); i++ {
+			if i == 0 && y == 0 {
+				x[i] = unicode.ToUpper(x[i])
+				upperStr += string(x[i])
+				continue
+			}
+			if xj.UpperCase {
+				x[i] = unicode.ToUpper(x[i])
+				upperStr += string(x[i])
+				continue
+			}
+			if xj.LowerCase {
+				x[i] = unicode.ToLower(x[i])
+				upperStr += string(x[i])
+				continue
+			}
+			if i == 0 {
+				x[i] = unicode.ToUpper(x[i])
+				upperStr += string(x[i])
+				continue
+			}
+
+			upperStr += string(x[i])
+			continue
+		}
+	}
+	return upperStr
+}
+
+// 4 char
+func (xj *xjson) XJSplit(r rune) bool {
+	return r == ':' || r == '.' || r == '-' || r == '_'
+}
